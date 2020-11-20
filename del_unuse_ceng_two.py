159f92a254138e388773792f22ec6fa5a023ce8c
FXTD-odyssey <2595715768@qq.com>
2020-11-20 17:21:03 +0800

    文件整理

--- del_unuse_ceng_two.py
+++
@@ -1,9 +0,0 @@
-#-*- coding:utf-8 -*-
-import maya.mel as mel
-def unuse():
-    #删除不使用的节点
-    mel.eval('hyperShadePanelMenuCommand("hyperShadePanel1", "deleteUnusedNodes");')
-    #删除不出不使用的动画层
-    mel.eval('deleteEmptyAnimLayers()')
-    #删除不使用的display层
-    mel.eval('layerEditorSelectUnused;layerEditorDeleteLayer""')
